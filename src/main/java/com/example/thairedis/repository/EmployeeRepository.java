d46cfd089bf408f5f8bf0fccaabeff0e296e4f4a
ThaiLe <levohuuthai1@gmail.com>
2021-11-08 11:41:03 +0700

    crud-list

--- src/main/java/com/example/thairedis/repository/EmployeeRepository.java
+++ src/main/java/com/example/thairedis/repository/EmployeeRepository.java
@@ -9,37 +9,53 @@ import java.util.List;
 
 @Repository
 public class EmployeeRepository {
-    private HashOperations hashOperations;//crud hash
+   // private HashOperations hashOperations;//crud hash
 
-
+    private ListOperations listOperations;
 
 
     private RedisTemplate redisTemplate;
 
     public EmployeeRepository(RedisTemplate redisTemplate) {
 
-        this.hashOperations = redisTemplate.opsForHash();
-        this.redisTemplate = redisTemplate;
+//        this.hashOperations = redisTemplate.opsForHash();
+//        this.redisTemplate = redisTemplate;
 
+        this.listOperations = redisTemplate.opsForList();
+        this.redisTemplate = redisTemplate;
     }
 
     public void saveEmployee(Employee employee){
 
-        hashOperations.put("EMPLOYEE", employee.getId(), employee);
+        //hashOperations.put("EMPLOYEE", employee.getId(), employee);
+        listOperations.rightPush("EMPLOYEE_LIST", employee);
     }
     public List<Employee> findAll(){
 
-        return hashOperations.values("EMPLOYEE");
+        //return hashOperations.values("EMPLOYEE");
+        return listOperations.range("EMPLOYEE_LIST", 0, -1);
+
+
+
     }
     public Employee findById(Integer id){
+        //return (Employee) hashOperations.get("EMPLOYEE", id);
 
-        return (Employee) hashOperations.get("EMPLOYEE", id);
+        List<Employee> list = findAll();
+        for(Employee e : list) {
+            if(e.getId() == id)
+                return e;
+        };
+        return null;
     }
 
     public void update(Employee employee){
         saveEmployee(employee);
     }
     public void delete(Integer id){
-        hashOperations.delete("EMPLOYEE", id);
+
+        //hashOperations.delete("EMPLOYEE", id);
+
+        listOperations.remove("EMPLOYEE_LIST", 1, findById(id));
     }
 }

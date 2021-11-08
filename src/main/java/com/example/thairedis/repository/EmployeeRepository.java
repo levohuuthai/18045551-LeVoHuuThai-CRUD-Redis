866447e65c96e68c8ab4740acb06d3ecc86d92ce
ThaiLe <levohuuthai1@gmail.com>
2021-11-08 11:47:48 +0700

    crud-set-commit

--- src/main/java/com/example/thairedis/repository/EmployeeRepository.java
+++ src/main/java/com/example/thairedis/repository/EmployeeRepository.java
@@ -3,16 +3,20 @@ import com.example.thairedis.model.Employee;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public class EmployeeRepository {
    // private HashOperations hashOperations;//crud hash
 
-    private ListOperations listOperations;
+    //private ListOperations listOperations;
 
+    private SetOperations setOperations;
 
     private RedisTemplate redisTemplate;
 
@@ -21,31 +25,45 @@ public class EmployeeRepository {
 //        this.hashOperations = redisTemplate.opsForHash();
 //        this.redisTemplate = redisTemplate;
 
-        this.listOperations = redisTemplate.opsForList();
+//        this.listOperations = redisTemplate.opsForList();
+//        this.redisTemplate = redisTemplate;
+
+        this.setOperations = redisTemplate.opsForSet();
         this.redisTemplate = redisTemplate;
     }
 
     public void saveEmployee(Employee employee){
 
         //hashOperations.put("EMPLOYEE", employee.getId(), employee);
-        listOperations.rightPush("EMPLOYEE_LIST", employee);
+
+        //listOperations.rightPush("EMPLOYEE_LIST", employee);
+
+        setOperations.add("EMPLOYEE_SET", employee);
     }
     public List<Employee> findAll(){
 
         //return hashOperations.values("EMPLOYEE");
-        return listOperations.range("EMPLOYEE_LIST", 0, -1);
 
+        //return listOperations.range("EMPLOYEE_LIST", 0, -1);
 
-
+        return Arrays.asList((Employee[]) setOperations.members("EMPLOYEE_SET").toArray());
     }
+
     public Employee findById(Integer id){
         //return (Employee) hashOperations.get("EMPLOYEE", id);
 
-        List<Employee> list = findAll();
-        for(Employee e : list) {
-            if(e.getId() == id)
-                return e;
-        };
+//        List<Employee> list = findAll();
+//        for(Employee e : list) {
+//            if(e.getId() == id)
+//                return e;
+//        };
+//        return null;
+
+        Set<Employee> employees = setOperations.members("EMPLOYEE");
+        for (Employee employee : employees) {
+            if(employee.getId() == id)
+                return employee;
+        }
         return null;
     }
 
@@ -56,6 +74,8 @@ public class EmployeeRepository {
 
         //hashOperations.delete("EMPLOYEE", id);
 
-        listOperations.remove("EMPLOYEE_LIST", 1, findById(id));
+       //listOperations.remove("EMPLOYEE_LIST", 1, findById(id));
+
+        setOperations.remove("EMPLOYEE_SET", findById(id));
     }
 }
